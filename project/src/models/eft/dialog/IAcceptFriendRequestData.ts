f2ef3efdacbf960eeb529c824b9bcfc5f6086fee
TheSparta <thesparta@noreply.dev.sp-tarkov.com>
2023-11-01 14:39:44 +0000

    fixed suppressions/unused

--- project/src/models/eft/dialog/IAcceptFriendRequestData.ts
+++ project/src/models/eft/dialog/IAcceptFriendRequestData.ts
@@ -1,11 +1,9 @@
-// biome-ignore lint/suspicious/noEmptyInterface: <explanation>
-export  interface IAcceptFriendRequestData extends IBaseFriendRequest
+export interface IAcceptFriendRequestData extends IBaseFriendRequest
 {
-    
+
 }
 
-// biome-ignore lint/suspicious/noEmptyInterface: <explanation>
-export  interface ICancelFriendRequestData extends  IBaseFriendRequest
+export interface ICancelFriendRequestData extends IBaseFriendRequest
 {
 
 }
