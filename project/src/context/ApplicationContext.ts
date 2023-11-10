5fa8803f8c5d82854a10016328610e404ffd3f61
Refringe <brownelltyler@gmail.com>
2023-11-10 15:23:51 -0500

    Auto-formatting for context classes.

--- project/src/context/ApplicationContext.ts
+++ project/src/context/ApplicationContext.ts
@@ -5,29 +5,29 @@ import { ContextVariableType } from "@spt-aki/context/ContextVariableType";
 import { LinkedList } from "@spt-aki/utils/collections/lists/LinkedList";
 
 @injectable()
-export class ApplicationContext 
+export class ApplicationContext
 {
     private variables = new Map<ContextVariableType, LinkedList<ContextVariable>>();
     private static holderMaxSize = 10;
 
     /**
-     * Called like:
-     * 
-     * const registerPlayerInfo = this.applicationContext.getLatestValue(ContextVariableType.REGISTER_PLAYER_REQUEST).getValue<IRegisterPlayerRequestData>();
-     * 
-     * const activePlayerSessionId = this.applicationContext.getLatestValue(ContextVariableType.SESSION_ID).getValue<string>();
-     * 
-     * const matchInfo = this.applicationContext.getLatestValue(ContextVariableType.MATCH_INFO).getValue<IStartOfflineRaidRequestData>();
-     * @param type 
-     * @returns 
+     * @examples
+     *
+     * const registerPlayerInfo = this.applicationContext.getLatestValue(ContextVariableType.REGISTER_PLAYER_REQUEST)
+     *     .getValue<IRegisterPlayerRequestData>();
+     *
+     * const activePlayerSessionId = this.applicationContext.getLatestValue(ContextVariableType.SESSION_ID)
+     *     .getValue<string>();
+     *
+     * const matchInfo = this.applicationContext.getLatestValue(ContextVariableType.MATCH_INFO)
+     *     .getValue<IStartOfflineRaidRequestData>();
      */
-    public getLatestValue(type: ContextVariableType): ContextVariable 
+    public getLatestValue(type: ContextVariableType): ContextVariable
     {
-        if (this.variables.has(type)) 
+        if (this.variables.has(type))
         {
             return this.variables.get(type)?.getTail()?.getValue();
         }
-
         return undefined;
     }
 
@@ -37,20 +37,25 @@ export class ApplicationContext
         {
             return this.variables.get(type).toList();
         }
-
         return undefined;
     }
 
-    public addValue(type: ContextVariableType, value: any): void 
+    public addValue(type: ContextVariableType, value: any): void
     {
         let list: LinkedList<ContextVariable>;
         if (this.variables.has(type))
+        {
             list = this.variables.get(type);
+        }
         else
+        {
             list = new LinkedList<ContextVariable>();
+        }
 
         if (list.getSize() >= ApplicationContext.holderMaxSize)
+        {
             list.removeFirst();
+        }
 
         list.add(new ContextVariable(value, type));
         this.variables.set(type, list);
