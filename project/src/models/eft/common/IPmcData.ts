78c53f36d898b543d127789dccfd87268f696ad2
Dev <dev@dev.sp-tarkov.com>
2023-12-30 10:45:37 +0000

    Improve post raid type data accuracy

--- project/src/models/eft/common/IPmcData.ts
+++ project/src/models/eft/common/IPmcData.ts
@@ -5,6 +5,12 @@ export interface IPmcData extends IBotBase
 
 export interface IPostRaidPmcData extends IBotBase
 {
+    Stats: IPostRaidStats
+}
+
+export interface IPostRaidStats
+{
+    Eft: IEftStats
     /** Only found in profile we get from client post raid */
-    EftStats: IEftStats;
+    Arena: IEftStats
 }
