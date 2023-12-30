a6843f994ef4e5429269bc6f79125134fe04ee72
Dev <dev@dev.sp-tarkov.com>
2023-12-30 16:45:09 +0000

    Revert "Update JSONs"
    This reverts commit 89c4dfde357643c8b8a1b95b76978325d2ff3fc8.

--- project/src/models/eft/common/tables/ITemplateItem.ts
+++ project/src/models/eft/common/tables/ITemplateItem.ts
@@ -86,7 +86,6 @@ export interface Props
     EffectiveDistance?: number;
     Ergonomics?: number;
     Velocity?: number;
-    WithAnimatorAiming?: boolean
     RaidModdable?: boolean;
     ToolModdable?: boolean;
     UniqueAnimationModID?: number;
@@ -184,17 +183,14 @@ export interface Props
     weapUseType?: string;
     ammoCaliber?: string;
     OperatingResource?: number;
-    PostRecoilHorizontalRangeHandRotation?: Ixyz
-    PostRecoilVerticalRangeHandRotation?: Ixyz
-    ProgressRecoilAngleOnStable?: Ixyz
     RepairComplexity?: number;
     durabSpawnMin?: number;
     durabSpawnMax?: number;
     isFastReload?: boolean;
     RecoilForceUp?: number;
     RecoilForceBack?: number;
+    Convergence?: number;
     RecoilAngle?: number;
-    RecoilCamera?: number
     weapFireType?: string[];
     RecolDispersion?: number;
     SingleFireRate?: number;
@@ -202,7 +198,6 @@ export interface Props
     bFirerate?: number;
     bEffDist?: number;
     bHearDist?: number;
-    blockLeftStance?: boolean
     isChamberLoad?: boolean;
     chamberAmmoCount?: number;
     isBoltCatch?: boolean;
@@ -211,9 +206,8 @@ export interface Props
     AdjustCollimatorsToTrajectory?: boolean;
     shotgunDispersion?: number;
     Chambers?: Slot[];
+    CameraRecoil?: number;
     CameraSnap?: number;
-    CameraToWeaponAngleSpeedRange?: Ixyz
-    CameraToWeaponAngleStep?: number
     ReloadMode?: string;
     AimPlane?: number;
     TacticalReloadStiffnes?: Ixyz;
@@ -221,7 +215,6 @@ export interface Props
     RecoilCenter?: Ixyz;
     RotationCenter?: Ixyz;
     RotationCenterNoStock?: Ixyz;
-    ShotsGroupSettings?: IShotsGroupSettings[]
     FoldedSlot?: string;
     CompactHandling?: boolean;
     MinRepairDegradation?: number;
@@ -253,11 +246,6 @@ export interface Props
     AllowOverheat?: boolean;
     DoubleActionAccuracyPenalty?: number;
     RecoilPosZMult?: number;
-    RecoilReturnPathDampingHandRotation?: number
-    RecoilReturnPathOffsetHandRotation?: number
-    RecoilReturnSpeedHandRotation?: number
-    RecoilStableAngleIncreaseStep?: number
-    RecoilStableIndexShot?: number
     MinRepairKitDegradation?: number;
     MaxRepairKitDegradation?: number;
     BlocksEarpiece?: boolean;
@@ -543,13 +531,4 @@ export interface IColor
     g: number;
     b: number;
     a: number;
-}
-
-export interface IShotsGroupSettings
-{
-    EndShotIndex: number
-    ShotRecoilPositionStrength: Ixyz
-    ShotRecoilRadianRange: Ixyz
-    ShotRecoilRotationStrength: Ixyz
-    StartShotIndex: number
 }
