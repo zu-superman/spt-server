cf490c841e00be4b851692324285af375ac4ce2f
Dev <dev@dev.sp-tarkov.com>
2024-03-23 10:42:43 +0000

    Create centralised reward blacklist

--- project/src/services/ItemFilterService.ts
+++ project/src/services/ItemFilterService.ts
@@ -32,6 +32,25 @@ export class ItemFilterService
     }
 
     /**
+     * Check if item is blacklisted from being a reward for player
+     * @param tpl item tpl to check is on blacklist
+     * @returns True when blacklisted
+     */
+    public isItemRewardBlacklisted(tpl: string): boolean
+    {
+        return this.itemConfig.rewardItemBlacklist.includes(tpl);
+    }
+
+    /**
+     * Get an array of items that should never be given as a reward to player
+     * @returns string array of item tpls
+     */
+    public getItemRewardBlacklist(): string[]
+    {
+        return this.itemConfig.rewardItemBlacklist;
+    }
+
+    /**
      * Return every template id blacklisted in config/item.json
      * @returns string array of blacklisted tempalte ids
      */
