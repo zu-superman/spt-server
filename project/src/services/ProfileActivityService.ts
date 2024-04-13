0d6239ea42ad130b18c48c8791bbc1529b3d6f60
Dev <dev@dev.sp-tarkov.com>
2024-04-13 23:04:17 +0100

    Added `getActiveProfileIdsWithinMinutes()` to `ProfileActivityService`

--- project/src/services/ProfileActivityService.ts
+++ project/src/services/ProfileActivityService.ts
@@ -26,6 +26,34 @@ export class ProfileActivityService
     }
 
     /**
+     * Get an array of profile ids that were active in the last x minutes
+     * @param minutes How many minutes from now to search for profiles
+     * @returns String array of profile ids
+     */
+    public getActiveProfileIdsWithinMinutes(minutes: number): string[]
+    {
+        const currentTimestamp = new Date().getTime() / 1000;
+        const result: string[] = [];
+
+        for (const id of Object.keys(this.profileActivityTimestamps ?? {}))
+        {
+            const lastActiveTimestamp = this.profileActivityTimestamps[id];
+            if (!lastActiveTimestamp)
+            {
+                continue;
+            }
+
+            // Profile was active in last x minutes, add to return list
+            if ((currentTimestamp - lastActiveTimestamp) < (minutes * 60))
+            {
+                result.push(id);
+            }
+        }
+
+        return result;
+    }
+
+    /**
      * Update the timestamp a profile was last observed active
      * @param sessionId Profile to update
      */
