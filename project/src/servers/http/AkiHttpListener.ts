7fd9dadb13e14f6b10279745eda56e90eaebf06c
Dev <dev@dev.sp-tarkov.com>
2024-04-20 14:19:35 +0100

    Revert "Split out debug heading into 2 headers, one each for request/response"
    This reverts commit e2f3191212c45ce23472aefe2678c9858c9ea32d.

--- project/src/servers/http/AkiHttpListener.ts
+++ project/src/servers/http/AkiHttpListener.ts
@@ -64,11 +64,10 @@ export class AkiHttpListener implements IHttpListener
                     // determine if the payload is compressed. All PUT requests are, and POST requests without
                     // debug = 1 are as well. This should be fixed.
                     // let compressed = req.headers["content-encoding"] === "deflate";
-                    const requestIsCompressed = req.headers.requestcompressed === "1";
-                    const requestCompressed = req.method === "PUT" || requestIsCompressed;
+                    const compressed = req.method === "PUT" || req.headers.debug !== "1";
 
-                    const value = requestCompressed ? zlib.inflateSync(buffer) : buffer;
-                    if (!requestIsCompressed)
+                    const value = compressed ? zlib.inflateSync(buffer) : buffer;
+                    if (req.headers.debug === "1")
                     {
                         this.logger.debug(value.toString(), true);
                     }
@@ -108,7 +107,7 @@ export class AkiHttpListener implements IHttpListener
         let handled = false;
 
         // Check if this is a debug request, if so just send the raw response without transformation
-        if (req.headers.responsecompressed === "0")
+        if (req.headers.debug === "1")
         {
             this.sendJson(resp, output, sessionID);
         }
