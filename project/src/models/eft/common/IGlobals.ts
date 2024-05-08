8821115cfcaa3f2d9d80c41de9644fbc5594a0cf
Refringe <me@refringe.com>
2024-05-08 00:26:12 -0400

    Fixes Duplicate Interface
    This renames a BTR configuration interface that had the same name as another interface.
    
    IBtrMapConfig --> IBtrPathConfig

--- project/src/models/eft/common/IGlobals.ts
+++ project/src/models/eft/common/IGlobals.ts
@@ -1120,7 +1120,7 @@ export interface IBtrMapConfig
     pathsConfigurations: IBtrMapConfig[]
 }
 
-export interface IBtrMapConfig
+export interface IBtrPathConfig
 {
     id: string
     enterPoint: string
