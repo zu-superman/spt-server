2a750a99893b0edb53abf44105b4535ada5e21d8
Dev <dev@dev.sp-tarkov.com>
2024-06-22 20:40:13 +0100

    Fixed regression where sellchance because NaN due to a divide by 0 error

--- project/src/helpers/ItemHelper.ts
+++ project/src/helpers/ItemHelper.ts
@@ -503,10 +503,10 @@ export class ItemHelper
     /**
      * Calcualte the average quality of an item and its children
      * @param items An offers item to process
-     * @param nonQualityItemsReturnNegative Treat items without a quality property as non-existant
+     * @param skipArmorItemsWithoutDurability Skip over armor items without durability
      * @returns % quality modifer between 0 and 1
      */
-    public getItemQualityModifierForItems(items: Item[], nonQualityItemsReturnNegative = false): number
+    public getItemQualityModifierForItems(items: Item[], skipArmorItemsWithoutDurability?: boolean): number
     {
         if (this.isOfBaseclass(items[0]._tpl, BaseClasses.WEAPON))
         {
@@ -517,7 +517,7 @@ export class ItemHelper
         let itemsWithQualityCount = 0;
         for (const item of items)
         {
-            const result = this.getItemQualityModifier(item, nonQualityItemsReturnNegative);
+            const result = this.getItemQualityModifier(item, skipArmorItemsWithoutDurability);
             if (result === -1)
             {
                 continue;
@@ -532,13 +532,24 @@ export class ItemHelper
 
     /**
      * get normalized value (0-1) based on item condition
+     * Will return -1 for base armor items with 0 durability
      * @param item
-     * @returns number between 0 and 1
+     * @param skipArmorItemsWithoutDurability return -1 for armor items that have maxdurability of 0
+     * @returns Number between 0 and 1
      */
-    public getItemQualityModifier(item: Item, nonQualityItemsReturnNegative = false): number
+    public getItemQualityModifier(item: Item, skipArmorItemsWithoutDurability?: boolean): number
     {
         // Default to 100%
         let result = 1;
+
+        // Is armor and has 0 max durability
+        const itemDetails = this.getItem(item._tpl)[1];
+        if (skipArmorItemsWithoutDurability
+          && this.isOfBaseclass(item._tpl, BaseClasses.ARMOR)
+          && itemDetails._props.MaxDurability === 0)
+        {
+            return -1;
+        }
 
         if (item.upd)
         {
@@ -548,8 +559,6 @@ export class ItemHelper
             const key = item.upd.Key ? item.upd.Key : undefined;
             const resource = item.upd.Resource ? item.upd.Resource : undefined;
             const repairKit = item.upd.RepairKit ? item.upd.RepairKit : undefined;
-
-            const itemDetails = this.getItem(item._tpl)[1];
 
             if (medkit)
             {
@@ -580,12 +589,6 @@ export class ItemHelper
             {
                 // Repair kits
                 result = repairKit.Resource / itemDetails._props.MaxRepairResource;
-            }
-
-            // Not handled by any of the above + default value
-            if (nonQualityItemsReturnNegative && result === 1)
-            {
-                return -1;
             }
 
             if (result === 0)
