d7ce092a4a21e2b2889187de1e4b6db573037888
Dev <dev@dev.sp-tarkov.com>
2024-08-29 17:36:31 +0100

    Added code to clean up insured items data post-raid

--- project/src/services/LocationLifecycleService.ts
+++ project/src/services/LocationLifecycleService.ts
@@ -589,6 +589,8 @@ export class LocationLifecycleService {
             this.insuranceService.storeGearLostInRaidToSendLater(sessionId, mappedItems);
 
             this.insuranceService.sendInsuredItems(preRaidPmcProfile, sessionId, locationName);
+
+            this.insuranceService.removeLostInsuranceFromProfile(preRaidPmcProfile, request.lostInsuredItems);
         }
     }
 
