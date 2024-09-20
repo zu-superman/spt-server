23570f4e2a033a819286022927ef8d1ea0b9bef2
Dev <dev@dev.sp-tarkov.com>
2024-09-20 16:43:00 +0100

    Hmm

--- project/src/services/LocationLifecycleService.ts
+++ project/src/services/LocationLifecycleService.ts
@@ -747,7 +747,7 @@ export class LocationLifecycleService {
 
             this.insuranceService.storeGearLostInRaidToSendLater(sessionId, mappedItems);
 
-            this.insuranceService.peePeePooPooFartPoo(preRaidPmcProfile, sessionId, locationName);
+            this.insuranceService.startPostRaidInsuranceLostProcess(preRaidPmcProfile, sessionId, locationName);
         }
     }
 
