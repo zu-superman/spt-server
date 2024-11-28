f55861528ad85322457791a47e86ab6fb6a7cf25
Refringe <me@refringe.com>
2024-11-28 10:51:53 -0500

    Compress Database Location Files (#954)
    Due to LFS storage issues... This PR removes all current LFS files (the
    location loot files) and replaces them with a single 7-zip archive. The
    archive is stored in LFS, but has decreased in size by roughly 95%.
    
    The location `.json` files are now git-ignored.
    
    There are two new npm commands to aid in working with the archive:
    - `npm run database:compress`  
    Compresses the JSON files into an archive *which can be committed into
    the project*.
    - `npm run database:decompress`  
    Decompresses the archive into the original JSON files located in the
    working directory.
    
    The gulp file that handles builds has been updated to ensure that the
    archive files are *always* used when a build is processed, regardless of
    if the JSON files are already present in the working directory.

--- project/src/di/Container.ts
+++ project/src/di/Container.ts
@@ -253,6 +253,7 @@ import { StaticRouterModService } from "@spt/services/mod/staticRouter/StaticRou
 import { App } from "@spt/utils/App";
 import { AsyncQueue } from "@spt/utils/AsyncQueue";
 import { CompareUtil } from "@spt/utils/CompareUtil";
+import { DatabaseDecompressionUtil } from "@spt/utils/DatabaseDecompressionUtil";
 import { DatabaseImporter } from "@spt/utils/DatabaseImporter";
 import { EncodingUtil } from "@spt/utils/EncodingUtil";
 import { HashUtil } from "@spt/utils/HashUtil";
@@ -419,6 +420,9 @@ export class Container {
     private static registerUtils(depContainer: DependencyContainer): void {
         // Utils
         depContainer.register<App>("App", App, { lifecycle: Lifecycle.Singleton });
+        depContainer.register<DatabaseDecompressionUtil>("DatabaseDecompressionUtil", DatabaseDecompressionUtil, {
+            lifecycle: Lifecycle.Singleton,
+        });
         depContainer.register<DatabaseImporter>("DatabaseImporter", DatabaseImporter, {
             lifecycle: Lifecycle.Singleton,
         });
